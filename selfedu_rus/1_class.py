b883445e1f3f189c29ebb0eceebc9d8b0c65bae3
Andru Kovalchuk <cinbek@yandex.ru>
2022-01-19 17:04:05 +0300

    advanced SOLID selfedu_rus __getatribute_getattr_setattr_delatr

--- selfedu_rus/1_class.py
+++ selfedu_rus/1_class.py
@@ -1,9 +1,21 @@
 #17.01.2022
-
+#видимость действия атрибутов класса
 class Point:
     color = "red"
     circle = 2
 
-print(getattr(Point, 'a', "Error "))
-
 a = Point()
+b = Point()
+
+a.x = 2
+a.y = 5
+a.z = 7
+b.x = 20
+b.y = 40
+b.z = 2
+
+print(a.__dict__, b.__dict__)
+for i in ([a.__dict__, b.__dict__]):
+    for s in i:
+        print(s, "=", i[s])
+    print()
