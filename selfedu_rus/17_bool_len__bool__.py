25666ae7d747f9d0893c7c2fa3dd3effccfea5fd
Andru Kovalchuk <cinbek@yandex.ru>
2022-01-23 07:39:59 +0300

    advanced SOLID selfedu_rus @_bool_len__bool__

---
+++ selfedu_rus/17_bool_len__bool__.py
@@ -0,0 +1,20 @@
+#23.01.2022
+
+class Point:
+    """ Принцип работы метода __bool__ если есть ф-ция __len__ она принмиается вначале
+        а вот если есть ф-ция __bool__ то она первоочередная"""
+
+    def __init__(self,x, y):
+        self.__x = x
+        self.__y = y
+
+    def __len__(self):
+        print("__len__")
+        return self.__x * self.__x + self.__y * self.__y
+
+    def __bool__(self):
+        print("__bool__")
+        return self.__x == self.__y
+
+p = Point(12, 12)
+print(bool(p))
