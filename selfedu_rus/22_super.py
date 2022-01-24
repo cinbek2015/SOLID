4cad55a7901855af880cdea958f1f67092763faf
Andru Kovalchuk <cinbek@yandex.ru>
2022-01-24 08:23:00 +0300

    advanced SOLID selfedu_rus @_super

---
+++ selfedu_rus/22_super.py
@@ -0,0 +1,19 @@
+#24.01.2022
+
+class Geom:
+    "класс демонстрирующий работу  super()"
+    def __init__(self, x, y):
+        print("__init__ Geom")
+        self.__x = x
+        self.__y = y
+        self._fill = 0
+
+class Line(Geom):
+
+    def __init__(self, x, y, fill=None):
+        super().__init__(x, y)
+        print("__init__ Line")
+        self._fill = fill
+
+l = Line(12, 24, 666)
+print(l.__dict__)
