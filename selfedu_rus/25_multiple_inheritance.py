c8760c0f31ad37eb25c100ee9af7d7dd1ac1941c
Andru Kovalchuk <cinbek@yandex.ru>
2022-01-24 12:01:22 +0300

    advanced SOLID selfedu_rus @_mro_multi_nasledovanie

---
+++ selfedu_rus/25_multiple_inheritance.py
@@ -0,0 +1,30 @@
+#24.01.2022
+
+class Shop:
+
+    def __init__(self, name, price, weight):
+        super().__init__()#
+        self._name = name
+        self._price = price
+        self._weight = weight
+
+    def get_shop(self):
+        return f'{self._name} : {self._price} Руб, weight = {self._weight}'
+
+class LogSales:
+    ID = 0
+
+    def __init__(self):
+        print("__init__ LogSales")
+        self.ID += 1
+        self.id = self.ID
+
+    def get_ID(self):
+        return self.ID
+
+class NoteBook(Shop, LogSales):
+    pass
+
+n1 = NoteBook("Asus GFT25J17sd", 46324, 17)
+print(n1.get_shop(), "log =", n1.get_ID())
+print(NoteBook.__mro__)
