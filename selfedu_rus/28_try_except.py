4faeea4e57043bf2f29995d98cfded32e12f96de
Andru Kovalchuk <cinbek@yandex.ru>
2022-01-25 11:00:05 +0300

    advanced SOLID selfedu_rus @_try_except

--- selfedu_rus/28_try_except.py
+++ selfedu_rus/28_try_except.py
@@ -8,5 +8,7 @@ except ValueError:
     print("Ошибка ввода данных")
 except ZeroDivisionError:
     print("Ошибка: деление на ноль")
+except:
+    print("Неопознанная ошибка, молитесь сволочи, я иду искать")
 
 print("Продолжение выполнения программы")
