36b6fd92e798af7afddec7290703160b299f9a3c
Andru Kovalchuk <cinbek@yandex.ru>
2022-01-27 08:16:57 +0300

    advanced SOLID selfedu_rus @_metaclass_

--- selfedu_rus/34_35_metaclass.py
+++ selfedu_rus/34_35_metaclass.py
@@ -1,11 +1,26 @@
 #26.01.2022
+#*****************************************************
+A = type("Point", (), {"Andru": 46, "Roma": 37})
+a = A()
+a.Gera = 36
+print("Пример простого создания метакласса, Example metaclass create simple")
+print(a.__dict__)
+print(a.Andru)
 
-# A = type("Point", (), {"Andru": 46, "Roma": 37})
-# a = A()
-# a.Gera = 36
-# print(a.__dict__)
-# print(a.Andru)
+#******************************************************
+#пример создание метокласса с помощью функции
+def metaclass_creator(name, base, attr):
+    #как пример можем добавлять по желанию два атрибута
+    attr.update({"MAX_COORD": 100, "MIN_COORD": 0})
+    return type(name, base, attr)
 
+class MyClass(metaclass=metaclass_creator):
+    def myfunc(self):
+        return (0, 0)
+
+mycl = MyClass()
+print("MyClass example", mycl.myfunc())
+#********************************************************
 #создаем метакласс
 class Meta(type):
 
@@ -28,3 +43,4 @@ class Point(metaclass=Meta):
 p = Point
 print(p.MIN_COORD)
 print(p.MAX_COORD)
+#****************************************************************
